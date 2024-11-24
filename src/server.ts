5851bfe30568df7f494e251ca1617fabe18dc06a
Rafael de Lima <rafalimabcl@gmail.com>
2024-11-23 23:36:25 -0300

    chore: updated server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import { Router, Request, Response } from "express";
 
 const app = express();
 
+const route = Router();
+
+app.use(express.json());
+
+route.get('/', (req: Request, res: Response) => {
+    res.json({message: "Sucess!"});
+});
+
+app.use(route);
+
 const PORT = 8080;
 
-app.listen(PORT, () => console.log("Server is running!"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
